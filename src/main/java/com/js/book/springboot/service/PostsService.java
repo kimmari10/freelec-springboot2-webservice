8e28617f3b01dbf71c53d13f7507a89ce1ddf33c
kimmari10 <kimmari10@naver.com>
2020-03-25 21:21:45 +0900

    게시글 저장 및 조회 API 구현 테스트

---
+++ src/main/java/com/js/book/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.js.book.springboot.service;
+
+import com.js.book.springboot.domain.posts.PostsRepository;
+import com.js.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
