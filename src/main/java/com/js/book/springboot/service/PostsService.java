63c04e23b3d66239905bcde639e55f735430e99b
kimmari10 <kimmari10@naver.com>
2020-03-25 22:09:54 +0900

    게시글 수정 API 테스트

--- src/main/java/com/js/book/springboot/service/PostsService.java
+++ src/main/java/com/js/book/springboot/service/PostsService.java
@@ -1,6 +1,8 @@
 package com.js.book.springboot.service;
 
+import com.js.book.springboot.domain.posts.Posts;
 import com.js.book.springboot.domain.posts.PostsRepository;
+import com.js.book.springboot.web.dto.PostsResponseDto;
 import com.js.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,5 +18,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsSaveRequestDto requestDto) {
+
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+
+        entity.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/js/book/springboot/web/PostsApiController.java
+++ src/main/java/com/js/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.js.book.springboot.web;
 
+import com.js.book.springboot.domain.posts.Posts;
 import com.js.book.springboot.service.PostsService;
+import com.js.book.springboot.web.dto.PostsResponseDto;
 import com.js.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,8 +15,17 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 }
