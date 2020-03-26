9c37e5d41d58dc1a7ebaa282295f45783e2b4637
kimmari10 <kimmari10@naver.com>
2020-03-26 22:14:52 +0900

    글쓰기, 리스트조회 추가 devtools 추가

--- src/main/java/com/js/book/springboot/service/PostsService.java
+++ src/main/java/com/js/book/springboot/service/PostsService.java
@@ -2,11 +2,16 @@ package com.js.book.springboot.service;
 
 import com.js.book.springboot.domain.posts.Posts;
 import com.js.book.springboot.domain.posts.PostsRepository;
+import com.js.book.springboot.web.dto.PostsListResponseDto;
 import com.js.book.springboot.web.dto.PostsResponseDto;
 import com.js.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -36,5 +41,12 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/js/book/springboot/web/IndexController.java
+++ src/main/java/com/js/book/springboot/web/IndexController.java
@@ -1,13 +1,29 @@
 package com.js.book.springboot.web;
 
+import com.js.book.springboot.service.PostsService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String posts() {
+        return "posts-save";
     }
 }
