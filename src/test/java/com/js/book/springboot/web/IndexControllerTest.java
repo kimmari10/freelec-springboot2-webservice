f4bde7705f9fdc2f63445451ec69f8257f11159a
kimmari10 <kimmari10@naver.com>
2020-03-26 21:03:44 +0900

    mustache index페이지 테스트

---
+++ src/test/java/com/js/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,24 @@
+package com.js.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void index() {
+        String body = testRestTemplate.getForObject("/", String.class);
+        assertThat(body).contains("스프링 부트");
+    }
+}
