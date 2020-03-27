e72a2d4d5a56b839d125e1a32bf3f66302bc1ff4
kimmari10 <kimmari10@naver.com>
2020-03-27 20:38:03 +0900

    수정화면 추가, 수정화면 Dto 바인딩

--- src/main/java/com/js/book/springboot/web/IndexController.java
+++ src/main/java/com/js/book/springboot/web/IndexController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RequiredArgsConstructor
@@ -23,7 +24,13 @@ public class IndexController {
     }
 
     @GetMapping("/posts/save")
-    public String posts() {
+    public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
     }
 }
