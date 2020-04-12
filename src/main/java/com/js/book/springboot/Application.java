c989ee8c6ed46fcc9622990d44626b73418c9eca
kimmari10 <kimmari10@naver.com>
2020-04-12 09:30:04 +0900

    kakaomap 마커 클릭이벤트 추가

--- src/main/java/com/js/book/springboot/Application.java
+++ src/main/java/com/js/book/springboot/Application.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource(value = {"classpath:/jdbc.properties"})
 public class Application {
     public static void main(String[] args) {
-        System.setProperty("spring.devtools.restart.enabled", "true");
-        System.setProperty("spring.devtools.livereload.enabled", "true");
         SpringApplication.run(Application.class, args);
     }
 }
