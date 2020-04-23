3b79eceec14e9bffe067e9beffcba37b9fd39903
kimmari10 <kimmari10@naver.com>
2020-04-24 01:12:19 +0900

    첨부 컨트롤러 생성

--- src/main/java/com/js/book/springboot/web/UploadController.java
+++ src/main/java/com/js/book/springboot/web/UploadController.java
@@ -4,17 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@RestController
 public class UploadController {
-//    @ResponseBody // json 형식으로 리턴
-//    @PostMapping(value = "/upload/uploadAjax", produces = "text/plain;charset=utf-8")
-//    public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception {
-//
-//        //new ResponseEntity (데이터, 상태코드)
-//        //new ResponseEntity (업로드된 파일이름, 상태코드)
-//
-//        return new ResponseEntity<String>(
-//                UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()), HttpStatus.OK);
-//    }
+    @PostMapping(value = "/upload/uploadAjax")
+    public void uploadAjax(MultipartFile file) throws Exception {
+        System.out.println(file.getOriginalFilename());
+
+    }
 }
