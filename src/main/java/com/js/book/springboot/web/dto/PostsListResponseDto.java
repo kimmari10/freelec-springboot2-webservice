69d9a479ffa78334f4cd79d86cd7a358e014c01d
kimmari10 <kimmari10@naver.com>
2020-04-30 02:18:48 +0900

    date format 적용

--- src/main/java/com/js/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/js/book/springboot/web/dto/PostsListResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @NoArgsConstructor
@@ -12,14 +13,14 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime modifiedDate;
+    private String modifiedDate;
     private String existFileYn;
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.modifiedDate = entity.getModifiedDate();
+        this.modifiedDate = entity.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.existFileYn = entity.getUploadFiles().size() > 0 ? "Y" : "N";
     }
 }
